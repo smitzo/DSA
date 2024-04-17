93da55c3a0b56ea85ff96ed60dba95b3f28fafda
Smit joshi <joshismit2812@gmail.com>
2024-04-17 12:45:57 +0530

    commit

--- 100XDevs-DSA/2.Arrays/Array_Problems/leetcode_arrays/1512.number-of-good-pairs.cpp
+++ 100XDevs-DSA/2.Arrays/Array_Problems/leetcode_arrays/1512.number-of-good-pairs.cpp
@@ -3,16 +3,13 @@
 Given an array of integers nums, return the number of good pairs.
 A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
-Input: nums = [1,2,3,1,1,3]
-Output: 4
+Input: nums = [1,2,3,1,1,3] Output: 4
 Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 
-Input: nums = [1,1,1,1]
-Output: 6
+Input: nums = [1,1,1,1] Output: 6
 Explanation: Each pair in the array aregood.
 
-Input: nums = [1,2,3]
-Output: 0
+Input: nums = [1,2,3] Output: 0
 
 Optimal solution
 Instead of running 2 loops, let’s see if we can do with one loop. We are running the loop of i. 
